19d0931e655d15282eb70fa132e09a78b87c248d
GalSajko <gal.sajko@gmail.com>
2023-05-23 14:36:33 +0200

    Setup file for building a wheel file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="gwpconfig",
+    packages=find_packages(include=["gwpconfig"]),
+    version="1.0.0",
+    description="Green-Wall-Project Python library with configuration modules for entire project.",
+    author="Gal Sajko, Jure Pavlovič",
+    license="MIT",
+)
