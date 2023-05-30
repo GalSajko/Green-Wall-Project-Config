0ac7db877f86cd30ee6b0b7c033714bfdde90d26
GalSajko <gal.sajko@gmail.com>
2023-05-30 14:29:12 +0200

    Changed version in setup.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="gwpconfig",
     packages=find_packages(include=["gwpconfig"]),
-    version="1.0.0",
+    version="1.0.1",
     description="Green-Wall-Project Python library with configuration modules for entire project.",
     author="Gal Sajko, Jure Pavlovič",
     license="MIT",
